74ff77f870f8d6247099d538b89ab8b3b81b86fe
Douglas Camargo <camargo.douglas@icloud.com>
2020-06-01 09:53:09 -0300

    starting ts server

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get('/users', (request, response) => {
+    
+    response.json([
+        "Hello World"
+    ]);
+});
+
+app.listen(3333);
